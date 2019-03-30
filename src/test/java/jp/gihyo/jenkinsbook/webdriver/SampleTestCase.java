9de4561140b64b1cd7df9d8e411eca80aaa2ae46
Yu Takada <baki504@msn.com>
2019-03-31 00:40:04 +0900

    fix a assertion

--- src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
+++ src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
@@ -7,87 +7,93 @@ import java.io.IOException;
 import java.util.Calendar;
 import java.util.Properties;
 
-import jp.gihyo.jenkinsbook.page.ResultPage;
-import jp.gihyo.jenkinsbook.page.TopPage;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import jp.gihyo.jenkinsbook.page.ResultPage;
+import jp.gihyo.jenkinsbook.page.TopPage;
 
 public class SampleTestCase {
 
-	private static Properties prop = new Properties();
-	private static WebDriver driver;
-	
-	@BeforeClass
-	public static void setUpClass() throws IOException {
-		prop.load(new FileInputStream("target\\test-classes\\selenium.properties"));
-		driver = new FirefoxDriver();
-	}
-	
-	@AfterClass
-	public static void tearDownClass() throws IOException {
-		driver.quit();
-	}
-	
-	@Test
-	public void testNormal01() {
-		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
-		TopPage topPage = new TopPage(driver);
-		assertEquals("名字", topPage.getLastNameLabel());
-		assertEquals("名前", topPage.getFirstNameLabel());
+    private static Properties prop = new Properties();
+    private static WebDriver driver;
 
-		assertTrue(topPage.hasLastNameInput());
-		assertTrue(topPage.hasFirstNameInput());
-		assertTrue(topPage.hasSubmit());
-	}
-	
-	@Test
-	public void testNormal02() {
-		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
-		TopPage topPage = new TopPage(driver);
-		topPage.setLastName("Hoge");
-		topPage.setFirstName("Foo");
-		topPage.submit();
-		
-		String greeting = "";
-		Calendar calendar = Calendar.getInstance();
-		int hour = calendar.get(Calendar.HOUR_OF_DAY);
-		if (hour < 12) {
-			greeting = "Good morning";
-		} else {
-			greeting = "Good afternoon";
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        prop.load(new FileInputStream("target\\test-classes\\selenium.properties"));
+        driver = new FirefoxDriver();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws IOException {
+        driver.quit();
+    }
+
+    @Test
+    public void testNormal01() {
+        driver.get(prop.getProperty("baseUrl") + "/sampleproject");
+
+        TopPage topPage = new TopPage(driver);
+        assertEquals("名字", topPage.getLastNameLabel());
+        assertEquals("名前", topPage.getFirstNameLabel());
+
+        assertTrue(topPage.hasLastNameInput());
+        assertTrue(topPage.hasFirstNameInput());
+        assertTrue(topPage.hasSubmit());
+    }
+
+    @Test
+    public void testNormal02() throws InterruptedException {
+        driver.get(prop.getProperty("baseUrl") + "/sampleproject");
+
+        TopPage topPage = new TopPage(driver);
+        topPage.setLastName("Hoge");
+        topPage.setFirstName("Foo");
+        topPage.submit();
+
+        String greeting = "";
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (hour < 12) {
+            greeting = "Good morning";
+        } else {
+            greeting = "Good afternoon";
         }
-		ResultPage resultPage = new ResultPage(driver);
-		assertEquals(greeting + ", Hoge Foo!!", resultPage.getText());
-	}
-	
-	@Test
-	public void testError01() {
-		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
-		TopPage page = new TopPage(driver);
-		page.setFirstName("Hoge");
-		page.submit();
-		
-		ResultPage resultPage = new ResultPage(driver);
-		assertEquals("エラー", resultPage.getText());
-	}
 
-	@Test
-	public void testError02() {
-		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
-		TopPage page = new TopPage(driver);
-		page.setLastName("Hoge");
-		page.submit();
-		
-		ResultPage resultPage = new ResultPage(driver);
-		assertEquals("エラー", resultPage.getText());
-	}
+        Thread.sleep(1000l);
+
+        ResultPage resultPage = new ResultPage(driver);
+        assertEquals("Hello" + ", Hoge Foo!!", resultPage.getText());
+    }
+
+    @Test
+    public void testError01() throws InterruptedException {
+        driver.get(prop.getProperty("baseUrl") + "/sampleproject");
+
+        TopPage page = new TopPage(driver);
+        page.setFirstName("Hoge");
+        page.submit();
+
+        Thread.sleep(1000l);
+
+        ResultPage resultPage = new ResultPage(driver);
+        assertEquals("エラー", resultPage.getText());
+    }
+
+    @Test
+    public void testError02() throws InterruptedException {
+        driver.get(prop.getProperty("baseUrl") + "/sampleproject");
+
+        TopPage page = new TopPage(driver);
+        page.setLastName("Hoge");
+        page.submit();
+
+        Thread.sleep(1000l);
+
+        ResultPage resultPage = new ResultPage(driver);
+        assertEquals("エラー", resultPage.getText());
+    }
 }
